396214d4e083c035c433f3bf35056d0033b83c86
Christopher Krügelstein <christopher.kruegelstein@nexenio.com>
2018-05-21 12:16:00 +0200

    created a new app for fruit ninja

---
+++ fruit-ninja/src/components/app/AppContainer.js
@@ -0,0 +1,12 @@
+// @flow
+
+import { connect } from "react-redux";
+
+// Components
+import App from "./App.jsx";
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
