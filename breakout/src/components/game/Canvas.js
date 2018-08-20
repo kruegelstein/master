a867d54b7e922e6ea6b87ff3f11f79451632b894
kruegelstein <CBB@ChristohersiMac.fritz.box>
2018-08-20 08:27:42 +0200

    outsourcing

--- breakout/src/components/game/Canvas.js
+++ breakout/src/components/game/Canvas.js
@@ -3,10 +3,11 @@
 import styled from "styled-components";
 
 const canvas = styled.canvas`
-  width: 60%;
-  height: 60%;
+  width: ${props => props.width};
+  height: ${props => props.height};;
+  display: ${props => props.userId ? 'initial' : 'none'}
   background-color: white;
-  margin: 10rem 0;
+  margin: 2rem 0;
 `;
 
 export default canvas;
