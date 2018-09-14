5007f7606e9a907f0ff43d152e08a1df3f4b0ae2
Christopher Krügelstein <christopher.kruegelstein@nexenio.com>
2018-09-14 18:04:04 +0200

    connection to firebase

--- simon/src/components/results/Results.jsx
+++ simon/src/components/results/Results.jsx
@@ -4,17 +4,22 @@ import React, { Component } from "react";
 import ResultsComp from "./Results.js";
 import Title from "../general/Title.js";
 import Paragraph from "../general/Paragraph.js";
+import Button from "../general/Button.js";
 
 class Results extends Component {
-  componentWillMount() {
-    this.props.sendResultsToDb(this.props.results, this.props.userId);
-  }
   render() {
     if (this.props.showResults) {
       return (
         <ResultsComp>
           <Title>Thank you for participating!</Title>
           <Paragraph>Your results have been saved</Paragraph>
+          <Button
+            onClick={() =>
+              this.props.sendResultsToDb(this.props.results, this.props.userId)
+            }
+          >
+            Submit
+          </Button>
         </ResultsComp>
       );
     }
