6011f3a2914aba6ff4053c8cb7ba6975d26e9a4c
Christopher Krügelstein <christopher.kruegelstein@nexenio.com>
2018-10-20 10:06:47 +0200

    only circles as elements

--- simon/src/utils/elements.js
+++ simon/src/utils/elements.js
@@ -20,55 +20,62 @@ export const createElements = numberToCreate => {
   let elements = [];
   let i = 0;
   for (i = 0; i < numberToCreate; i++) {
-    if (Math.floor(Math.random() * 2) === 0) {
-      elements.push(
-        <Square
-          active={false}
-          key={generateID()}
-          color={
-            theme.baseColors[
-              Object.keys(theme.baseColors)[Math.floor(Math.random() * 6)]
-            ]
-          }
-          size={theme.sizes.xl}
-          // This will generate squares of random size
-          // size={
-          //   theme.sizes[Object.keys(theme.sizes)[Math.floor(Math.random() * 4)]]
-          // }
-          margin={theme.margin.large}
-          // This will generate squares with random margin
-          // margin={
-          //   theme.margin[
-          //     Object.keys(theme.margin)[Math.floor(Math.random() * 3)]
-          //   ]
-          // }
-        />
-      );
-    } else {
-      elements.push(
-        <Circle
-          active={false}
-          key={generateID()}
-          color={
-            theme.baseColors[
-              Object.keys(theme.baseColors)[Math.floor(Math.random() * 6)]
-            ]
-          }
-          size={theme.sizes.xl}
-          // This will generate circles of random size
-          // size={
-          //   theme.sizes[Object.keys(theme.sizes)[Math.floor(Math.random() * 4)]]
-          // }
-          margin={theme.margin.large}
-          // This will generate circles with random margin
-          // margin={
-          //   theme.margin[
-          //     Object.keys(theme.margin)[Math.floor(Math.random() * 3)]
-          //   ]
-          // }
-        />
-      );
-    }
+    elements.push(
+      <Circle
+        active={false}
+        key={generateID()}
+        color={
+          theme.baseColors[
+            Object.keys(theme.baseColors)[Math.floor(Math.random() * 6)]
+          ]
+        }
+        size={theme.sizes.xl}
+        // This will generate circles of random size
+        // size={
+        //   theme.sizes[Object.keys(theme.sizes)[Math.floor(Math.random() * 4)]]
+        // }
+        margin={theme.margin.large}
+        // This will generate circles with random margin
+        // margin={
+        //   theme.margin[
+        //     Object.keys(theme.margin)[Math.floor(Math.random() * 3)]
+        //   ]
+        // }
+      />
+    );
   }
   return elements;
 };
+
+/*
+If we want to create a Square element use:
+<Square
+  active={false}
+  key={generateID()}
+  color={
+    theme.baseColors[
+      Object.keys(theme.baseColors)[Math.floor(Math.random() * 6)]
+    ]
+  }
+  size={theme.sizes.xl}
+  // This will generate squares of random size
+  // size={
+  //   theme.sizes[Object.keys(theme.sizes)[Math.floor(Math.random() * 4)]]
+  // }
+  margin={theme.margin.large}
+  // This will generate squares with random margin
+  // margin={
+  //   theme.margin[
+  //     Object.keys(theme.margin)[Math.floor(Math.random() * 3)]
+  //   ]
+  // }
+/>
+
+Also we can generate them randomly with:
+if (Math.floor(Math.random() * 2) === 0) {
+  elements.push(<Circle.../>)
+} else {
+  elements.push(<Square.../>)
+}
+
+*/
