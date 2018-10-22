46737c71f4fde81d8908ded7884d4b7bd25476e5
Christopher Krügelstein <christopher.kruegelstein@nexenio.com>
2018-10-22 08:50:14 +0200

    new styles for userIdInput

--- breakout/src/components/app/App.js
+++ breakout/src/components/app/App.js
@@ -3,11 +3,9 @@
 import styled from "styled-components";
 
 const app = styled.div`
-  background-color: black;
-  height: 100vh;
-  width: 100vw;
-  color: white;
-  text-align: center;
+  background-color: white;
+  height: 100%;
+  width: 100%;
 `;
 
 export default app;
